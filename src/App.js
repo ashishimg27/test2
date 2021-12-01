377b988b7e3114a23a66257850d39a2db3b6116d
ashishimg27 <95354816+ashishimg27@users.noreply.github.com>
2021-12-01 20:27:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+// import PostList from './components/PostList'
+import PostForm from './components/PostForm'
+function App() {
+  return (
+    <div>
+   {/* <PostList/> */}
+   <PostForm/>
+    </div>
+  );
+}
+
+export default App;
